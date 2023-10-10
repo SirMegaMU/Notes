ef9576d9df1f9f20948561cffeae2a5834765b83
MegaMU <sirmegamu@outlook.com>
2023-10-10 13:34:28 +0800

    update

---
+++ WIA1002_Data_Structure/App1.java
@@ -0,0 +1,38 @@
+class Person {
+    String name;
+    int age;
+
+    void teaching(){
+        for (int i = 0; i<3; i++){
+            System.out.println("My name is: "+ name + ", age " + age);
+        }
+        System.out.println("Hello");
+    }
+    void calculateYearToGraduate(){
+        double yearsLeft = age + 3.5;
+        System.out.println(yearsLeft);
+    }
+    int getAge(){
+        return age;
+    }
+}
+
+public class App1{
+    public static void main(String[] args){
+        String Address, name;
+
+        Person person1 = new Person();
+        person1.name = "Zaquan";
+        person1.age = 30;
+        Person person2 = new Person();
+        person2.name = "Fatimah";
+        person2.age = 12;
+
+        System.out.println(person2.name+"  "+person1.age);
+        person2.teaching();
+        // person1.calculateYearToGraduate();
+
+        int newage = person1.getAge();
+        System.out.println(newage);
+    }
+}
